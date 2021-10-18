81b6ee3dd5c2964e61f4b9364e5123254da6c69d
Aheuf <clement.genty@gmail.com>
2021-10-18 09:15:02 +0200

    compilation of the import in the component

--- src/App.js
+++ src/App.js
@@ -3,31 +3,13 @@ import React, {useState} from 'react';
 import FileUploader from './components/FileUploader';
 import axios from 'axios';
 
-const App = () => {
-  const [name, setName] = useState("");
-  const [selectedFile, setSelectedFile] = useState(null);
-
-  const submitForm = () => {
-    const formData = new FormData();
-    formData.append("name",name);
-    formData.append("file", selectedFile);
-
-    axios
-      .post("/components",formData)
-      .then((res) => {alert("File Upload success");
-    })
-      .catch((err) => alert("File Upload Error"));
-  };
+function App() {
 
   return (
     <div className="App container">
         <h1>App-Tab</h1>
         <div>
-          <form action="#" className="my-5">
-              <input type="text" className="form-control" value={name} onChange= {(e) => setName(e.target.value)} disabled/>
-              <FileUploader onFileSelectSuccess={(file) => setSelectedFile(file)} onFileSelectError={({error}) => alert(error)}/>
-              <button onClick={submitForm} className="btn btn-secondary">Submit</button>
-          </form>
+              <FileUploader />
         </div>
     </div>
   );
--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,22 +1,34 @@
-import React, {useRef} from "react"
+import React, {useState,useRef} from "react";
 
-const FileUploader = ({onFileSelect}) => {
+const FileUploader = () => {
+    const [name, setName] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
     const fileInput = useRef(null)
 
     const handleFileInput = (e) => {
         const file = e.target.files[0];
-        console.log(file);
-       /* if (file.size > 1024){
-            onFileSelectError({error:"File size cannot exceed more than 1MB"});
-        }else {
-            onFileSelectSuccess(file);
-        }*/
+        setName(file.name);
+        setSelectedFile(file);
     }
 
+    const submitForm = () => {
+        console.log(name);
+        console.log(selectedFile);
+        console.log(JSON.stringify(selectedFile));
+        const formData = new FormData();
+        formData.append("name",name);
+        formData.append("file", selectedFile);
+      };
+
     return (
-        <div className="file-uploader group-form">
-            <input type="file" onChange={handleFileInput} />
-            <button onClick={e => fileInput.current && fileInput.current.click()} >Envoyer</button>
+        <div className="FileUploader">
+            <div className="file-uploader input-group col-10 mt-5">
+                <input type="file" className="form-control col-10" onChange={handleFileInput} />
+                <input type="submit" className="btn btn-outline-secondary col-2" onClick={(file) => setSelectedFile(file), 
+                                                                                (file) => setName(file.name),
+                                                                                 e => fileInput.current && fileInput.current.click(),
+                                                                                 submitForm} />
+            </div>
         </div>
     )
 }
