672e3cd98818f464baa830476009c7c6c8e75789
Aheuf <clement.genty@gmail.com>
2021-10-18 17:12:50 +0200

    Change : minification of FileUploader.js

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,33 +1,20 @@
-import React, {useState,useRef} from "react";
+import React from "react";
 
 const FileUploader = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const fileInput = useRef(null)
-
-    const handleFileInput = (e) => {
-        const file = e.target.files[0];
-        setSelectedFile(file);
-        console.log("selectedFile avant submit : ");
-        console.log(selectedFile);
-    }
-
-    const submitForm = () => {
-        console.log("selectedFile après submit : ");
-        console.log(selectedFile);
-      /*  const formData = new FormData();
-        formData.append("file", selectedFile);*/
-      };
+    let file
+    const handleFileInput = (event) => {
+        file = event.target.files[0];
+        console.log(file);
+        console.log(JSON.stringify(file));
+    };
 
     return (
         <div className="FileUploader">
-            <div className="file-uploader input-group col-10 mt-5">
+            <div className="file-uploader col-10 mt-5">
                 <input type="file" className="form-control col-10" onChange={handleFileInput} />
-                <input type="submit" className="btn btn-outline-secondary col-2" onClick={(file) => setSelectedFile(file), 
-                                                                                 e => fileInput.current && fileInput.current.click(),
-                                                                                 submitForm} />
             </div>
         </div>
-    )
+    );
 }
 
 export default FileUploader;
