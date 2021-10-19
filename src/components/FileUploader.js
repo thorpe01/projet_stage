a50d3a6f1a7b08582d851198db1cd342871cb00f
Aheuf <clement.genty@gmail.com>
2021-10-19 22:03:42 +0200

    ADD : import csv | convert to json

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -4,16 +4,48 @@ const FileUploader = ({setData}) => {
 
     const handleFileInput = (event) => {
             let file = event.target.files[0];
+            let extension = file.name.split('.')[1];
 
-            var fileReader = new FileReader();
-            fileReader.onload = function (event) {setData(JSON.parse(event.target.result))};
-            fileReader.readAsText(file);
+            if (extension ==="csv"){
+                convertToJson(file);
+            }
+
+            if (extension === 'json'){
+                var fileReader = new FileReader();
+                fileReader.onload = function (event) {setData(JSON.parse(event.target.result))};
+                fileReader.readAsText(file);
+            } else if (extension !== 'csv'){
+                file = null;
+                alert("format inconnu, veuillez inserer un JSON ou un CSV");
+            }
     };
+
+    const convertToJson = (file) => {
+        var fileReader = new FileReader();
+
+        fileReader.onload = function (file) {
+            let lines = file.target.result.split("\n");
+            let result = [];
+            let headers = lines[0].split(";");
+
+            for (let i=1; i<lines.length;i++){
+                let obj = {};
+                let currentLine = lines[i].split(";");
+
+                for(let j=0 ;j < headers.length ; j++){
+                    obj[headers[j]] = currentLine[j];
+                }
+                result.push(obj);
+            }
+            setData(JSON.parse(JSON.stringify(result)));
+        };
+        fileReader.readAsText(file);
+    }
 
     return (
         <div className="FileUploader">
             <div className="file-uploader col-10 mt-5">
-                <input type="file" className="form-control col-10" onChange={handleFileInput} />
+                <input type="file" className="form-control col-10" accept=".json, .csv"onChange={handleFileInput} />
             </div>
         </div>
     );
