afa8904f60756c6909556d05fb76d06a5da6080a
Aheuf <clement.genty@gmail.com>
2021-10-20 17:46:44 +0200

    Refactor : scinder les composant front | back

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-import React, {useState} from 'react';
-import FileUploader from './components/FileUploader';
+import React from 'react';
+import Home from './components/Home';
 
 function App() {
-  const [data, setData] = useState(null);
 
     return (
       <div className="App container">
         <h1>App-Tab</h1>
         <div>
-          <FileUploader setData={setData}/>
+          <Home />
         </div>
       </div>
     );
