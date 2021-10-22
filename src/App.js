508a54aa4009bc1c3e906d980dd254804cf62180
thorpe01 <exercice0007@gmail.com>
2021-10-22 10:20:17 +0200

    recherche et filtre

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import './App.css';
+import React from 'react';
+import Search from './components/Search';
 
 
-function App() {
+
+const initialDetails = [
+    {
+      id: 1,
+      name: "Jane D",
+      email: "janedoe@gmail.com",
+      address: "New Delhi, India",
+    },
+    {
+      id: 2,
+      name: "Mary Rosamund",
+      email: "agra@rosie.com",
+      address: "Tbilisi, India",
+    },
+    {
+      id: 3,
+      name: "edouard michelin ",
+      email: "irene@johnlock.com",
+      address: "Baker Street, India",
+    },
+    {
+      id: 4,
+      name: "jules mac ",
+      email: "mary@johnlock.com",
+      address: "Baker Street, India",
+    },
+    {
+      id: 5,
+      name: "Mac",
+      email: "britishgovt@gmail.com",
+      address: "London, India",
+    },
+  ];
+
+
+ const App = () =>{
   return (
-    <div className="App">
-      <h1>App-Tab</h1>
-           
+    <div className="tc ma0 pa4 min-vh-100">
+      <Search details={initialDetails}/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
